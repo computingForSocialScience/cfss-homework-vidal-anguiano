2a0fe2f2ccfe0c183c937da27af261364aec215e
U-Vidal-PC\Vidal <vidal-anguiano@github.com>
2015-02-13 03:01:20 -0600

    main

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -1,12 +1,43 @@
 import sys
 from fetchArtist import fetchArtistId, fetchArtistInfo
-from fetchAlbums import fetchAlbumIds, fetchAlbumInfo
+from fetchAlbums import fetchAlbumInfo
+from albumtest import fetchAlbumIds
 from csvUtils import writeArtistsTable, writeAlbumsTable
 from barChart import plotBarChart
 
 if __name__ == '__main__':
     artist_names = sys.argv[1:]
     print "input artists are ", artist_names
-    # YOUR CODE HERE
+    
+    artist_ids_list = []
+
+    for artist in artist_names:
+    	artist_id = fetchArtistId(artist)
+    	artist_ids_list.append(artist_id)
+
+        artist_dicts_list = []
+
+    	for artist_id in artist_ids_list:
+            artist_info = fetchArtistInfo(artist_id)
+            artist_dicts_list.append(artist_info)
+
+    	writeArtistsTable(artist_dicts_list)
+
+    	album_ids_list = []
+
+    	for artist in artist_ids_list:
+    		album_ids_list += fetchAlbumIds(artist)
+
+    	album_dicts_list = []
+
+    	for album in album_ids_list:
+    		album_dicts_list.append(fetchAlbumInfo(album))
+
+    	writeAlbumsTable(album_dicts_list)
+
+    plotBarChart()
+
+
+
     
 
