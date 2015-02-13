7fd789635cdae9fe173d404e073625130f0a4f40
U-Vidal-PC\Vidal <vidal-anguiano@github.com>
2015-02-13 03:00:22 -0600

    Yep

--- Assignment5/csvUtils.py
+++ Assignment5/csvUtils.py
@@ -7,8 +7,21 @@ def writeArtistsTable(artist_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY
     """
-    pass 
-      
+    writer = open('artists.csv','w',encoding='utf-8')
+
+    writer.write(u'ARTIST_ID,ARTIST_NAME,ARTIST_FOLLOWERS,ARTIST_POPULARITY\n')
+    for artist in artist_info_list:
+        artistlist = artist_info_list[artist]
+        artistname = artistlist['name']
+        artistid = artistlist['id']
+        followers = artistlist['followers']
+        popularity = artistlist['popularity']
+        writer.write("%s,\"%s\",%d,%d\n" % (artistid,artistname,followers,popularity))
+
+
+    writer.close()
+
+
 def writeAlbumsTable(album_info_list):
     """
     Given list of dictionaries, each as returned
@@ -18,4 +31,20 @@ def writeAlbumsTable(album_info_list):
     The csv file should have a header line that looks like this:
     ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY
     """
-    pass
+    writer = open('albums.csv','w',encoding='utf-8')
+
+    writer.write(u'ARTIST_ID,ALBUM_ID,ALBUM_NAME,ALBUM_YEAR,ALBUM_POPULARITY\n')
+    for x in range(len(album_info_list)):
+        albumlist = album_info_list[x]
+        artistname = albumlist['artist_name']
+        artistid = albumlist['artist_id']
+        albumid = albumlist['album_id']
+        albumname = albumlist['album_name']
+        albumyear = int(albumlist['release_year'])
+        albumpopularity = albumlist['popularity']
+
+        writer.write('%s,%s,"%s",%d,%d\n' % (artistid,albumid,albumname,albumyear,albumpopularity))
+
+
+    writer.close()
+
