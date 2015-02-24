03edc3ab1648f99735dc08907e6398d4b53006f5
U-Vidal-PC\Vidal <vidal-anguiano@github.com>
2015-02-24 06:41:44 -0600

    Finished

--- Assignment5/fetchAlbums.py
+++ Assignment5/fetchAlbums.py
@@ -1,26 +1,26 @@
 import requests
 from datetime import datetime
 
-#def fetchAlbumIds(artist_id):
-#    """Using the Spotify API, take an artist ID and 
-#    returns a list of album IDs in a list
-#    """
-#    url = 'https://api.spotify.com/v1/artists/' + artist_id + '/albums?market=US&album_type=album'
-#    req = requests.get(url)
-#    data = req.json()
-#    list_of_album_ids = []
-#    for album in data['items']:
-#       list_of_album_ids.append(album['id'])
+def fetchAlbumIds(artist_id):
+    """Using the Spotify API, take an artist ID and 
+    returns a list of album IDs in a list
+    """
+    url = 'https://api.spotify.com/v1/artists/' + artist_id + '/albums?market=US&album_type=album'
+    req = requests.get(url)
+    data = req.json()
+    list_of_album_ids = []
+    for album in data['items']:
+       list_of_album_ids.append(album['id'])
 
-#    return list_of_album_ids
+    return list_of_album_ids
 
 
 def fetchAlbumInfo(album_id):
     """Using the Spotify API, take an album ID 
     and return a dictionary with keys 'artist_id', 'album_id' 'name', 'year', popularity'
     """
-    url1 = 'https://api.spotify.com/v1/albums/' + album_id
-    req1 = requests.get(url)
+    url = 'https://api.spotify.com/v1/albums/' + album_id
+    req = requests.get(url)
     album = req.json()
 
     albuminfo = {}
